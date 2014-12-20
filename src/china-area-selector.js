74fff4bb9f89f1f20cfa75e97939ce900a878353
blogbbs <blogbbs@gmail.com>
2014-12-20 15:04:56 +0800

    重构，使其适合更多场景

--- src/china-area-selector.js
+++ src/china-area-selector.js
@@ -7,41 +7,73 @@ angular.module('china-area-selector', ['templates'])
             scope: {
                 region: '='
             },
-            link: function (scope) {
+            link: function (scope, elements) {
+                var $region = angular.element(elements[0]);
+                var $city = angular.element($region.find('select')[1]);
+                var $area = angular.element($region.find('select')[2]);
+
                 scope.provinces = __areaData__.provinces || [];
 
-                scope.$watch('region.province', function(province, old) {
-                    scope.citys = (__areaData__[province] || {}).citys || [];
-                    
-                    if (province == old) {
-                        var temp = scope.region.city;
-                        scope.region.city = '';
-                        setTimeout(function() {
-                            scope.$apply(function() {
-                                scope.region.city = temp;
-                            });
-                        });
-                    } else {
-                        scope.region.city = '';
-                    }
-                });
+                var getCities = function(province) {
+                    return (__areaData__[province] || {}).citys || [];
+                };
 
-                scope.$watch('region.city', function(city, old) {
+                var getAreas = function(province, city) {
+                    var cityObj = (__areaData__[province] || {})[city];
+                    return (cityObj || {}).areas || [];
+                };
 
-                    var cityObj = (__areaData__[scope.region.province] || {})[city];
-                    scope.areas = (cityObj || {}).areas || [];
-                    if (city == old) {
-                        var temp = scope.region.area;
-                        scope.region.area = '';
-                        setTimeout(function() {
-                            scope.$apply(function() {
-                                scope.region.area = temp;
-                            });
-                        });
-                    } else {
-                        scope.region.area = '';
+                var indexOf = function(arr, ele) {
+                    for (var i in arr)
+                        if (arr[i] === ele)
+                            return i;
+                    return -1;
+                };
+
+                var getAdjustiveRegion = function(paramRegion) {
+                    var region = angular.copy(paramRegion || {});
+                    if (indexOf(scope.provinces, region.province) === -1) {
+                        region.province = region.city = region.area = '';
+                        return region;
                     }
-                });
+                    var cities = getCities(region.province);
+                    if (indexOf(cities, region.city) === -1) {
+                        region.city = region.area = '';
+                        return region;
+                    }
+                    var areas = getAreas(region.province, region.city);
+                    if (indexOf(areas, region.area) === -1) {
+                        region.area = '';
+                        return region;
+                    }
+                    return region;
+                };
+
+                var equalRegion = function(a, b) {
+                    if (!a || !b) {
+                        return false;
+                    }
+                    return (a.province === b.province) && (a.city === b.city) && (a.area === b.area);
+                }
+
+                var showRegion = function(region) {
+                    scope.region = getAdjustiveRegion(region);
+                    // console.log("region=" + JSON.stringify(region) + " scope.region=" + JSON.stringify(scope.region));
+
+                    scope.citys = getCities(scope.region.province);
+                    scope.areas = getAreas(scope.region.province, scope.region.city);
+
+                    setTimeout(function() {
+                        var cityIndex = indexOf(scope.citys, scope.region.city);
+                        $city.val(cityIndex > -1 ? cityIndex : '');
+
+                        var areaIndex = indexOf(scope.areas, scope.region.area);
+                        $area.val(areaIndex > -1 ? areaIndex : '');
+                    });
+                };
+
+                scope.$watch('region', showRegion, true);
+
             }
         };
     }]);
